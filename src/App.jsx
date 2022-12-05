9570ec0f9d7221cbf6083fb967b13fd905d479b8
Simona Zozuľaková <szozulakova@gmail.com>
2022-12-05 17:24:47 +0100

    awaiting fetch in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
 import { useEffect, useState } from "react";
-import { FetchBeers } from "./services/beerService";
+import { getBeers } from "./services/beerService";
 
 function App() {
   const [data, setData] = useState([]);
 
+  useEffect(() => {
+    const dataRetrieve = async () => {
+      const beers = await getBeers();
+
+      if (beers) {
+        setData(beers);
+      }
+    };
+    dataRetrieve();
+  }, []);
   return (
     <div>
-      <div>Hello</div>
-      <FetchBeers />
+      {data.map((beer) => (
+        <li>{beer.name}</li>
+      ))}
     </div>
   );
 }
--- src/services/beerService.jsx
+++ src/services/beerService.jsx
@@ -1,25 +1,5 @@
-import { useEffect, useState } from "react";
-
-export const FetchBeers = () => {
-  const [data, setData] = useState([]);
-
-  const fetchData = () => {
-    fetch("https://api.punkapi.com/v2/beers?per_page=30")
-      .then((response) => response.json())
-      .then((data) => {
-        setData(data);
-      });
-  };
-
-  useEffect(() => {
-    fetchData();
-  }, []);
-
-  return (
-    <div>
-      {data.map((beer) => (
-        <li>{beer.name}</li>
-      ))}
-    </div>
-  );
+export const getBeers = () => {
+  return fetch("https://api.punkapi.com/v2/beers?per_page=30")
+    .then((response) => response.json())
+    .then((data) => data);
 };
